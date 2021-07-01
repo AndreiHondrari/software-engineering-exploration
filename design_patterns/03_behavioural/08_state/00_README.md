b4f90bba033f01744371f4919a83b9355e248288
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-07-01 15:51:03 +0200

    DP 03/08 state.

--- design_patterns/03_behavioural/08_state/00_README.md
+++ design_patterns/03_behavioural/08_state/00_README.md
@@ -1,7 +1,18 @@
-# Design Patterns - 
+# Design Patterns - State
 
 ## Overview
 
+Instance changes behaviour based on its state.
+
 ## Use cases
 
+- TCP connection behaves differently based on its connectivity state
+- NPC (Non-player character) in game changes aggression behaviour based on
+its state in relation to you the human player
+
 ## Dissection
+
+**State** - interface defining what concrete states operations should be
+**Concrete state** - implements the state functionality
+**Context** - holds the current state and delegates function calls to the state
+instance
--- design_patterns/03_behavioural/08_state/f01_dt_oo_formal.py
+++ design_patterns/03_behavioural/08_state/f01_dt_oo_formal.py
@@ -18,7 +18,7 @@ class State(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def doing_that(self) -> None:
+    def do_that(self) -> None:
         raise NotImplementedError
 
     # Context-altering methods
@@ -39,3 +39,43 @@ class StateX(State):
 
     def do_something_altering(self, context: StateContext) -> None:
         print("[SX] doing altering stuff")
+        context.change_state(StateY())
+
+
+class StateY(State):
+    def do_this(self) -> None:
+        print("[SY] doing this")
+
+    def do_that(self) -> None:
+        print("[SY] doing that")
+
+    def do_something_altering(self, context: StateContext) -> None:
+        print("[SY] doing altering stuff")
+        context.change_state(StateX())
+
+
+class ConcreteContext(StateContext):
+
+    def foo(self) -> None:
+        print("Fooing")
+        self._state.do_this()
+        self._state.do_that()
+
+    def bar(self) -> None:
+        print("Baring")
+        self._state.do_something_altering(self)
+
+
+if __name__ == '__main__':
+    initial_state = StateX()
+    context = ConcreteContext(initial_state)
+    # from state X
+    context.foo()
+    context.bar()  # switches state to Y
+
+    # from state Y
+    context.foo()
+    context.bar()  # switches state to X
+
+    # from state X
+    context.foo()
