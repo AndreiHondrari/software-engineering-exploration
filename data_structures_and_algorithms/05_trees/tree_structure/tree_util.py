d390fe55f8dd4fcbf3ba6073a8c523ebc393bbc6
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-11-01 08:55:43 +0100

    Better tree structure visually.

--- data_structures_and_algorithms/05_trees/tree_structure/tree_util.py
+++ data_structures_and_algorithms/05_trees/tree_structure/tree_util.py
@@ -1,9 +1,18 @@
-from typing import Optional, Dict, Tuple
+import enum
+from typing import Optional, Dict, Tuple, List, DefaultDict
+from collections import defaultdict
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
 from .node import Node
+
+
+@enum.unique
+class NodeSide(enum.IntEnum):
+    ROOT = enum.auto()
+    LEFT = enum.auto()
+    RIGHT = enum.auto()
 
 
 def add_left_child(node: Node, value: int) -> Node:
@@ -25,7 +34,9 @@ def node_representation(
     prepend: str = "",
     orientation: str = ""
 ) -> str:
-    node_repr: str = f"{prepend}{orientation} [{node.value}]: {node.control_id}\n"
+    node_repr: str = (
+        f"{prepend}{orientation} [{node.value}]: {node.control_id}\n"
+    )
 
     prepend = "  " if (prepend == "") else (prepend + "  ")
 
@@ -43,7 +54,8 @@ def node_representation(
 def _construct_nx_tree(
     g: nx.Graph,
     node: Optional[Node],
-    side: int = 0,
+    side: NodeSide = NodeSide.ROOT,
+    level: int = 0
 ) -> None:
     if node is None:
         return
@@ -55,45 +67,92 @@ def _construct_nx_tree(
     nx.set_node_attributes(g, {
         node.control_id: {
             'data': node.value,
-            'side': side
+            'side': side,
+            'level': level,
+            'parent': (
+                node.parent.control_id if node.parent is not None else None
+            )
         }
     })
 
-    # fake nodes for empty
-    if node.left is None:
-        fake_left = Node(value="", parent=node)
-        g.add_node(fake_left.control_id)
-        g.add_edge(node.control_id, fake_left.control_id)
-    if node.right is None:
-        fake_right = Node(value="", parent=node)
-        g.add_node(fake_right.control_id)
-        g.add_edge(node.control_id, fake_right.control_id)
-
-    _construct_nx_tree(g, node.left, 1)
-    _construct_nx_tree(g, node.right, 2)
+    _construct_nx_tree(g, node.left, NodeSide.LEFT, level + 1)
+    _construct_nx_tree(g, node.right, NodeSide.RIGHT, level + 1)
 
 
-def _compute_positions(g) -> Dict[int, Tuple[int, int]]:
-    result = {}
+def _max_level_for_node(
+    node: Node,
+    level: int = 0,
+    max_level: int = 0
+) -> int:
 
-    return result
+    left_max: int = 0
+    right_max: int = 0
+    if node.left is not None:
+        left_max = _max_level_for_node(node.left, level + 1, max_level)
+
+    if node.right is not None:
+        right_max = _max_level_for_node(node.right, level + 1, max_level)
+
+    return max(level, max(left_max, right_max))
 
 
-def draw_tree(
+def _compute_positions(
+    g: nx.DiGraph,
     root: Node,
-    fig_scale: int = 1,
-    fig_scale_exponent: float = 1.2
-) -> None:
+    max_level: int
+) -> Dict[int, Tuple[int, int]]:
+
+    positions: Dict[int, Tuple[int, int]] = {}
+
+    node_sides: Dict[int, NodeSide] = nx.get_node_attributes(g, 'side')
+    node_levels: Dict[int, int] = nx.get_node_attributes(g, 'level')
+    node_parents: Dict[int, Optional[int]] = (
+        nx.get_node_attributes(g, 'parent')
+    )
+
+    # group nodes by level
+    nodes_by_level: DefaultDict[int, List[int]] = defaultdict(list)
+    for n in g:
+        nodes_by_level[node_levels[n]].append(n)
+
+    nodes_by_level.default_factory = None
+
+    # process node positions
+    for level in nodes_by_level:
+        for n in nodes_by_level[level]:
+            parent: Optional[int] = node_parents[n]
+            node_side: NodeSide = node_sides[n]
+
+            parent_x: int = positions[parent][0] if parent is not None else 0
+
+            y = -level
+            if node_side == NodeSide.LEFT:
+                x = parent_x - (2**(max_level - level + 1) // 2)
+            elif node_side == NodeSide.RIGHT:
+                x = parent_x + (2**(max_level - level + 1) // 2)
+            else:
+                x = 0
+
+            positions[n] = (x, y,)
+
+    return positions
+
+
+def draw_tree(root: Node, fig_scale: int = 1) -> None:
     g = nx.DiGraph()
     _construct_nx_tree(g, root)
 
-    positions = _compute_positions(g)
+    max_level: int = _max_level_for_node(root)
+    positions = _compute_positions(g, root, max_level)
+    total_node_count: int = 2**max_level
 
     # prepare graph drawing
+    INCH_FACTOR = 3 * fig_scale
     fig = plt.figure(frameon=False)
-    INCH_FACTOR = 5  # inches
-    fig_scale = int(fig_scale ** fig_scale_exponent)
-    fig.set_size_inches(fig_scale * INCH_FACTOR, fig_scale * INCH_FACTOR)
+    fig.set_size_inches(
+        INCH_FACTOR * (total_node_count / (max_level + 1)),
+        INCH_FACTOR
+    )
 
     labels = nx.get_node_attributes(g, 'data')
 
@@ -101,12 +160,12 @@ def draw_tree(
     color_map = []
     for n in g:
         node_side: int = nodes_sides.get(n, 99)
-        if node_side == 0:
+        if node_side == NodeSide.ROOT:
             color_map.append("red")
-        elif node_side == 1:
-            color_map.append("green")
-        elif node_side == 2:
-            color_map.append("blue")
+        elif node_side == NodeSide.LEFT:
+            color_map.append("lime")
+        elif node_side == NodeSide.RIGHT:
+            color_map.append("cyan")
         else:
             color_map.append("#e3e3e3")
 
@@ -115,9 +174,9 @@ def draw_tree(
         g,
         with_labels=True,
         labels=labels,
-        node_size=fig_scale**2 * 600,
-        # node_color='#e5e5e5',
+        node_size=600 * fig_scale,
         node_color=color_map,
-        font_weight='bold', font_size=str(10 * fig_scale),
-        pos=nx.drawing.nx_pydot.pydot_layout(g, prog='dot')
+        font_weight='bold',
+        font_size=str(10 * fig_scale),
+        pos=positions
     )
